9d73f9a61c6a822b78a1672d2e55cd190f9c0e51
Max <max.white.rabbit@gmail.com>
2019-05-01 16:43:38 +0500

    intro: map

--- intro/0.2types.cpp
+++ intro/0.2types.cpp
@@ -15,7 +15,13 @@ int main()
     string hw = "Hello world!";
     vector<int> nums = {1,3,5,7};
     cout << nums.size();
+    cout << '\n';
     
+    map<string, int> name_to_val;
+    name_to_val["one"] = 1;
+    name_to_val["two"] = 2;
+
+    cout << "two is " << name_to_val["two"];
     
     
     cout << '\n';
