b20a5425747fb33ea00e5d1b5324fd0b0c862c9f
Max <max.white.rabbit@gmail.com>
2019-05-01 16:28:45 +0500

    intro: hw and types

---
+++ intro/0.2types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    int x = -5;
+    double pi = 3.1516926;
+    bool logic = false;
+    char symb = 'Z';
+
+    string hw = "Hello world!";
+    vector<int> nums = {1,3,5,7};
+    cout << nums.size();
+    
+    
+    
+    cout << '\n';
+
+    return 0;
+}
---
+++ intro/hw.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "HW!\n";
+    return 0;
+}
