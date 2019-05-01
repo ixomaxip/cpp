0ddac5f1d93a85458725f0da9fb3bb416602fe69
Max <max.white.rabbit@gmail.com>
2019-05-01 21:50:11 +0500

    .

--- intro/0.5_cin_cout.cpp
+++ intro/0.5_cin_cout.cpp
@@ -7,13 +7,10 @@ using namespace std;
 
 int main()
 {
-    int x = 4;
-    string y = "hello";
-    cout << x << ' ' << y;
+    int x, y;
+    cin >> x >> y;
+    cout << x + y;
 
-    vector<int> d = {1,3};
-
-    cout << d;
     cout << '\n';
     return 0;
 }
