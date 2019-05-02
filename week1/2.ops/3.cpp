8124c3a493e3d02c9754561d4f94da7e8925d33a
Max <max.white.rabbit@gmail.com>
2019-05-02 14:20:12 +0500

    3 - logical

---
+++ week1/2.ops/3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    string a = "fire";
+    string b = "water";
+
+    if (a < b)
+    {
+        cout << "a is less than b" << endl;
+    }
+
+    return 0;
+}
