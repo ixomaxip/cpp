e6bd23608bcb81b8d721241e956bdb4d0857e8cb
Max <max.white.rabbit@gmail.com>
2019-05-02 23:10:10 +0500

    task3 - sq eq

---
+++ week1/tasks/03_equation.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a, b, c;
+    cin >> a >> b >> c;
+
+    if (a == 0 && b != 0)
+    {
+        cout << c / b  << endl;
+    }
+    else
+    {
+        double d = b*b - 4 * a * c;
+        if (d > 0)
+        {
+            cout << 0.5 * (- b + sqrt(d)) / a << 0.5 * (- b - sqrt(d)) / a << endl;
+        }
+        else if (d == 0 && (a != 0))
+        {
+            cout << - 0.5 * b / a << endl;
+        }
+    }
+
+    return 0;
+}
