3d8bcf0ff279433a0c6f245a861ec1b3f95b89e1
Max <max.white.rabbit@gmail.com>
2019-05-04 23:35:15 +0500

    .

---
+++ W2/1.functions/5_const_ref2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    const int x =5;
+    // x = 6;
+    // x +=4;
+
+    cout << x << endl;
+    return 0;
+}
