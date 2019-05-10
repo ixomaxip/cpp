ce6066219fa530ba78a7eb8f21e67360c7a60689
Max <max.white.rabbit@gmail.com>
2019-05-10 15:42:39 +0500

    .

--- W2/3.map/2.cpp
+++ W2/3.map/2.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <map>
+#include <vector>
 using namespace std;
 
 void print_map(const map<string, int> & ev)
 {
+    cout << "size = " << ev.size() << endl;
     for (const auto& item : ev)
     {
         cout << item.first << ": " << item.second << endl;
@@ -11,10 +13,22 @@ void print_map(const map<string, int> & ev)
 }
 int main()
 {
-    map <string, int> m = {{"one", 1}, {"two", 2}, {"three", 3}};
+    // map <string, int> m = {{"one", 1}, {"two", 2}, {"three", 3}};
 
-    print_map(m);
-    m.erase("three");
-    print_map(m);
+    // print_map(m);
+    // m.erase("three");
+    // print_map(m);
+
+    vector<string> words = {"one", "two", "one"};
+    map<string, int> cnt;
+    for (const auto & w : words)
+    {
+        print_map(cnt);
+        ++cnt[w];
+    }
+
+    print_map(cnt);
+
+
     return 0;
 }
