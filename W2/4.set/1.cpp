5aede43116251d0b70536dbd5f7778633afd0fa0
Max <max.white.rabbit@gmail.com>
2019-05-11 19:02:20 +0500

    set erase

--- W2/4.set/1.cpp
+++ W2/4.set/1.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void print_set(const set<string>& s)
 {
+    cout << "Size = " << s.size() << endl;
     for (auto x : s)
     {
         cout << x << endl;
@@ -17,7 +18,12 @@ int main()
     is_fam.insert("Stroustrup");
     is_fam.insert("Stroustrup");
     is_fam.insert("Ritchie");
+    is_fam.insert("Bla");
 
     print_set(is_fam);
+    cout << endl;
+    is_fam.erase("Bla");
+    print_set(is_fam);
+
     return 0;
 }
