971d568310f969ae32dd9700a61bdc2cdeba5de9
Max <max.white.rabbit@gmail.com>
2019-05-11 18:57:55 +0500

    set

---
+++ W2/4.set/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+#include <map>
+using namespace std;
+
+void print_set(const set<string>& s)
+{
+    for (auto x : s)
+    {
+        cout << x << endl;
+    }
+}
+
+int main()
+{
+    set<string> is_fam;
+    is_fam.insert("Stroustrup");
+    is_fam.insert("Ritchie");
+
+    print_set(is_fam);
+    return 0;
+}
