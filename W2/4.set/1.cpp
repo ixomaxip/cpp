ffa5c9ea680384f87be8a6a5086e133b477cfdae
Max <max.white.rabbit@gmail.com>
2019-05-11 23:17:06 +0500

    .

--- W2/4.set/1.cpp
+++ W2/4.set/1.cpp
@@ -25,5 +25,10 @@ int main()
     is_fam.erase("Bla");
     print_set(is_fam);
 
+    set<string> months = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+    set<string> months2 = {"JUN", "JUL", "AUG", "SEP", "OCT", "JAN", "FEB", "MAR", "APR", "MAY", "NOV", "DEC"};
+    print_set(months);
+    cout << (months == months2) << endl;
+
     return 0;
 }
