36ecdd10eb20c1094912b91dfca677f34fee0ef2
Max <max.white.rabbit@gmail.com>
2019-05-12 12:55:26 +0500

    algorithms from std

--- W3/01.Algorithms/1.cpp
+++ W3/01.Algorithms/1.cpp
@@ -5,22 +5,6 @@
 #include <iomanip>
 using namespace std;
 
-int min(int a, int b)
-{
-    if (a < b)
-        return a;
-    else
-        return b;
-}
-
-int max(int a, int b)
-{
-    if (a > b)
-        return a;
-    else
-        return b;
-}
-
 int main()
 {
     cout << min(2,3) << endl;
