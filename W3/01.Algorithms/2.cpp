d7ab388f79e7fb3f367826095b96900f9b94a415
Max <max.white.rabbit@gmail.com>
2019-05-12 15:39:29 +0500

    lambda

--- W3/01.Algorithms/2.cpp
+++ W3/01.Algorithms/2.cpp
@@ -15,23 +15,13 @@ void print(const vector<int>& v, const string& title )
     cout << endl;
 }
 
-bool gt2(int x)
-{
-    return x > 2;
-}
-bool lt2(int x)
-{
-    return x < 2;
-}
 int main()
 {
     vector<int> v { 1,3,2,5,4,2};
     int cnt = 0;
 
-    cout << count_if(begin(v), end(v), gt2) << endl;
-    cout << count_if(begin(v), end(v), lt2) << endl;
-
-
+    cout << count_if(begin(v), end(v), [](int x ) {return x > 2;}) << endl;
+    cout << count_if(begin(v), end(v), [](int x ) {return x < 2;}) << endl;
 
     return 0;
 }
