6f12685df7c7b17019baa1224ee8afabde26c5b3
Max <max.white.rabbit@gmail.com>
2019-05-14 00:36:25 +0500

    case-insensitive string sort - answer

--- W3/tasks/01.Algorithms/02.string_sort.cpp
+++ W3/tasks/01.Algorithms/02.string_sort.cpp
@@ -28,10 +28,20 @@ int main()
     vector<string> v(N);
 
     for (auto& i : v) {cin >> i;}
+
     sort(begin(v), end(v), [](const string& s1, const string& s2)
     {
         return s_tolower(s1) < s_tolower(s2);
     });
+
+    // sort(begin(v), end(v),
+    //     [](const string& l, const string& r)
+    //     {
+    //         return lexicographical_compare(begin(l), end(l), begin(r), end(r),
+    //             [](char cl, char cr) { return tolower(cl) < tolower(cr); });
+    //     }
+    // );
+
     for (const auto& i : v)
     {
         cout << i << " ";
