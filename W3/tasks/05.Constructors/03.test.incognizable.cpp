7217041530bb5cfeec16a627576166f40a0680ac
Max <max.white.rabbit@gmail.com>
2019-05-19 19:37:53 +0500

    train: incognizable

---
+++ W3/tasks/05.Constructors/03.test.incognizable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include "./03.train.incognizable.cpp"
+
+using namespace std;
+
+int main() {
+    Incognizable a;
+    Incognizable b = {};
+    Incognizable c = {0};
+    Incognizable d = {0, 1};
+    return 0;
+}
---
+++ W3/tasks/05.Constructors/03.train.incognizable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+using namespace std;
+
+class Incognizable
+{
+public:
+    // Incognizable() {};
+    Incognizable(int a = 0, int b = 0) {};
+
+};
