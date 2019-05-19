1f81d8fbe0069094e04a7e6e4bd666934a763b83
Max <max.white.rabbit@gmail.com>
2019-05-20 00:06:46 +0500

    aux structures

--- W4/01.StructAndClassesExamples/1.cpp
+++ W4/01.StructAndClassesExamples/1.cpp
@@ -2,11 +2,31 @@
 
 using namespace std;
 
+struct Day
+{
+    int value;
+};
+struct Month
+{
+    int value;
+};
+struct Year
+{
+    int value;
+};
+
 struct Date
 {
     int day;
     int month;
     int year;
+
+    Date(Day new_day, Month new_month, Year new_year)
+    {
+        day = new_day.value;
+        month = new_month.value;
+        year = new_year.value;
+    }
 };
 
 void prt_date(const Date& dt)
@@ -17,7 +37,7 @@ void prt_date(const Date& dt)
 
 int main()
 {
-    Date dt = {1,1,2017};
+    Date dt = {Day{10}, Month{11},Year{12}};
     prt_date(dt);
     return 0;
 }
