e81f538a56c1eb35e42726add2e6ee0c44444f65
Max <max.white.rabbit@gmail.com>
2019-05-21 22:41:27 +0500

    prototype step 2

--- W4/01.StructAndClassesExamples/2.cpp
+++ W4/01.StructAndClassesExamples/2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -16,11 +17,26 @@ struct Params
     double c;
 };
 
+class FunctionPart
+{
+
+};
+
+class Function
+{
+public:
+    void AddPart(char op, double value);
+    double Apply(double value) const;
+    void Invert();
+private:
+    vector<FunctionPart> parts;
+};
+
 Function make_weight_function(const Params& params, const Image& image)
 {
     Function func;
-    function.AddPart('-', image.freshness * params.a + params.b);
-    function.AddPart('+', image.rating * params.c);
+    func.AddPart('-', image.freshness * params.a + params.b);
+    func.AddPart('+', image.rating * params.c);
     return func;
 }
 
@@ -28,7 +44,7 @@ double compute_image_weight(const Params& params, const Image& image)
 {
     Function func = make_weight_function(params, image);
     return func.Apply(image.quality);
-    
+
     // double weight = image.quality;
     // weight -= image.freshness * params.a + params.b;
     // weight += image.rating * params.c;
