4cf3d8df1fc452e337357a11845d4041eef2f73a
Max <max.white.rabbit@gmail.com>
2019-05-21 22:26:15 +0500

    add image rating

--- W4/01.StructAndClassesExamples/2.cpp
+++ W4/01.StructAndClassesExamples/2.cpp
@@ -6,18 +6,21 @@ struct Image
 {
     double quality;
     double freshness;
+    double rating;
 };
 
 struct Params
 {
     double a;
     double b;
+    double c;
 };
 
-double cmpute_image_weight(const Params& params, const Image& image)
+double compute_image_weight(const Params& params, const Image& image)
 {
     double weight = image.quality;
     weight -= image.freshness * params.a + params.b;
+    weight += image.rating * params.c;
     return weight;
 }
 
