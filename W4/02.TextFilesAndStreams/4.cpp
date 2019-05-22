31431698de5426d29de9d8ed41f12988043e6ffb
Max <max.white.rabbit@gmail.com>
2019-05-22 23:16:08 +0500

    formatting output

---
+++ W4/02.TextFilesAndStreams/4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    vector<string> names = {"a", "b", "c"};
+    vector<double> vels = {5, 0.01, 0.00005};
+
+    for (const auto& n : names)
+    {
+        cout << n << ' ';
+    }
+    cout << endl;
+    cout << fixed;
+    for (const auto& n : vels)
+    {
+        cout << n << ' ';
+    }
+    cout << endl;
+    
+
+    return 0;
+}
