036f03225c2157c59798e6d95e0409ff96e6b64e
Max <max.white.rabbit@gmail.com>
2019-05-23 23:53:59 +0500

    operator+ -  first implementation

---
+++ W4/03.OperatorOverloading/2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+struct Duration
+{
+    int hour;
+    int min;
+
+    Duration(int h = 0, int m = 0)
+    {
+        hour = h;
+        min = m;
+    }
+};
+
+Duration read_duration(istream& stream)
+{
+    int h = 0;
+    int m = 0;
+    stream >> h;
+    stream.ignore(1);
+    stream >> m;
+
+    return Duration {h, m};
+}
+
+ostream& operator<<(ostream& stream, const Duration& dur)
+{
+    stream << setfill('0');
+    stream << setw(2) << dur.hour << ':'
+           << setw(2) << dur.min;
+    return stream;
+}
+
+istream& operator>>(istream& stream, Duration& dur)
+{
+    stream >> dur.hour;
+    stream.ignore(1);
+    stream >> dur.min;
+    return stream;
+}
+
+Duration operator+(const Duration& lhs, const Duration& rhs)
+{
+    return Duration(lhs.hour + rhs.hour, lhs.min + rhs.min);
+}
+
+int main()
+{
+    stringstream dur_ss("03:50");
+    Duration d1;
+    Duration d2 = {0, 5};
+    dur_ss >> d1;
+    cout << d1 + d2 << endl;
+    return 0;
+}
