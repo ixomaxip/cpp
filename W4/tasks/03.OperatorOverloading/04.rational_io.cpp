28a7bc6c7d847cb19995f4114ed5cafde3c6dc85
Max <max.white.rabbit@gmail.com>
2019-05-26 00:49:53 +0500

    task rational 4 - operators >> <<

--- W4/tasks/03.OperatorOverloading/04.rational_io.cpp
+++ W4/tasks/03.OperatorOverloading/04.rational_io.cpp
@@ -3,12 +3,104 @@
 using namespace std;
 
 class Rational {
-    // Вставьте сюда реализацию класса Rational из первой части
+public:
+    Rational() : num(0), den(1) {}
+
+    Rational(int numerator, int denominator)
+    {        
+        if (denominator < 0)
+        {
+            numerator *= -1;
+            denominator *= -1;
+        }
+        int gd = gcd(abs(numerator), abs(denominator));
+        num = numerator / gd;
+        den = denominator / gd;
+    }
+
+    int Numerator() const
+    {
+        return num;
+    }
+
+    int Denominator() const
+    {
+        return den;
+    }
+
+private:
+    int num;
+    int den;
+    int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+        if (b == 0 || b == a)
+            return a;
+        if (a == 1 || b == 1)
+            return 1;
+        if (a % 2 == 0 && b % 2 == 0 )
+            return 2 * gcd(a /2, b / 2);
+        if (a % 2 == 0 && b % 2 != 0)
+            return gcd(a / 2, b);
+        if (a % 2 != 0 && b % 2 == 0)
+            return gcd(a, b / 2);
+        if (a % 2 != 0 && b % 2 != 0 && a < b)
+            return gcd((b - a) / 2, a);
+        if (a % 2 != 0 && b % 2 != 0 && a > b)
+            return gcd((a - b) / 2, b);
+    }
 };
 
-// Вставьте сюда реализацию operator == для класса Rational из второй части
+bool operator== (const Rational& lhs, const Rational& rhs)
+{
+    return (lhs.Numerator() == rhs.Numerator()) && (lhs.Denominator() == rhs.Denominator());
+}
 
-// Реализуйте для класса Rational операторы << и >>
+Rational operator+ (const Rational& lhs, const Rational& rhs)
+{
+    Rational result {lhs.Numerator() * rhs.Denominator() + rhs.Numerator() * lhs.Denominator(),
+                     lhs.Denominator() * rhs.Denominator()};
+    return result;
+}
+
+Rational operator- (const Rational& lhs, const Rational& rhs)
+{
+    Rational result {lhs.Numerator() * rhs.Denominator() - rhs.Numerator() * lhs.Denominator(),
+                     lhs.Denominator() * rhs.Denominator()};
+    return result;
+}
+
+Rational operator* (const Rational& lhs, const Rational& rhs)
+{
+    Rational result {lhs.Numerator() * rhs.Numerator(),
+                     lhs.Denominator() * rhs.Denominator()};
+    return result;
+}
+
+Rational operator/ (const Rational& lhs, const Rational& rhs)
+{
+    Rational result {lhs.Numerator() * rhs.Denominator(),
+                     lhs.Denominator() * rhs.Numerator()};
+    return result;
+}
+
+ostream& operator<<(ostream& stream, const Rational& rat)
+{
+    stream << rat.Numerator() << "/" << rat.Denominator();
+    return stream;
+}
+
+istream& operator>>(istream& stream, Rational& rat)
+{
+    int num = rat.Numerator();
+    int den = rat.Denominator();
+    stream >> num;
+    stream.ignore(1);
+    stream >> den;
+    rat = {num, den};
+    return stream;
+}
 
 int main() {
     {
