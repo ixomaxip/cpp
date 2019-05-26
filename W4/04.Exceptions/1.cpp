6cb8e81daa258933f39da51b397729c50870246d
Max <max.white.rabbit@gmail.com>
2019-05-26 16:41:49 +0500

    .

--- W4/04.Exceptions/1.cpp
+++ W4/04.Exceptions/1.cpp
@@ -4,7 +4,32 @@
 #include <iomanip>
 #include <exception>
 
+using namespace std;
+
+struct Date
+{
+    int year;
+    int month;
+    int day;
+};
+
+Date parse_date(const string& s)
+{
+    stringstream stream(s);
+    Date dt;
+    stream >> dt.year;
+    stream.ignore(1);
+    stream >> dt.month;
+    stream.ignore(1);
+    stream >> dt.day;
+    stream.ignore(1);
+    return dt;
+}
+
 int main()
 {
+    string date_str = "2017/01/25";
+    Date dt = parse_date(date_str);
+    cout << dt.day << "-" << dt.month << "-" << dt.year << endl;
     return 0;
 }
