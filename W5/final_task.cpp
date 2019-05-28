90adb0a8063f74e0b9ad88b6c0a4f830af7cff89
Max <max.white.rabbit@gmail.com>
2019-05-29 00:02:50 +0500

    final task: template

---
+++ W5/final_task.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Date {
+public:
+  int GetYear() const;
+  int GetMonth() const;
+  int GetDay() const;
+};
+
+bool operator<(const Date& lhs, const Date& rhs);
+
+class Database {
+public:
+  void AddEvent(const Date& date, const string& event);
+  bool DeleteEvent(const Date& date, const string& event);
+  int  DeleteDate(const Date& date);
+
+  void Find(const Date& date) const;
+  
+  void Print() const;
+};
+
+int main() {
+  Database db;
+    
+  string command;
+  while (getline(cin, command))
+  {
+  }
+
+  return 0;
+}
