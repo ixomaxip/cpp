dc54714dd962d6b8513157c17ee2dcb098457db8
Max <max.white.rabbit@gmail.com>
2020-03-29 02:30:07 +0500

    Diagonal Difference

---
+++ hackerrank/diagonal_diff.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+
+// Diagonal Difference
+
+
+int diagonalDifference(vector<vector<int>> arr) {
+    int diff = 0;
+    for (size_t i = 0; i < arr.size(); i++) {
+        diff += arr[i][i] - arr[i][arr.size() - i - 1];
+    }
+    return abs(diff);
+}
+
+int main() {
+    vector<vector<int>> v = {{11,2,4},{4,5,6},{10,8,-12}};
+    cout << diagonalDifference(v) << endl;
+    
+
+    return 0;
+}
