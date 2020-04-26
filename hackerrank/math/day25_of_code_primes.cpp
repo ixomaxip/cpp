665b0e85ca08236425bb4c594728742427ed8446
Max <max.white.rabbit@gmail.com>
2020-04-27 00:03:29 +0500

    primes: really fast!

--- hackerrank/math/day25_of_code_primes.cpp
+++ hackerrank/math/day25_of_code_primes.cpp
@@ -49,16 +49,37 @@ bool is_prime(int n) {
 
 }
 
+
+bool is_prime2(int n) {
+    // naive: n = 2038074743 : 8471ms    
+    if (n <= 3) {
+        return n > 1;
+    }
+
+    if (n % 2 == 0 || n % 3 == 0) {
+        return false;
+    }
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            cout << i << endl;
+            return false;
+        }
+    }    
+    return true;
+
+}
+
 int main() {
     // vector<int> ns = read_numbers();
     vector<int> ns = {2038074743};
 
     for (const auto& n : ns) {
-        auto start = steady_clock::now();
-        is_prime(n) ? cout <<"Prime" << endl :
-                      cout <<"Not prime" << endl;
-        auto finish = steady_clock::now();
-        cout << "n = " << n << " : " << duration_cast<milliseconds>(finish - start).count() << "ms" << endl;
+        auto start = high_resolution_clock::now();
+        is_prime2(n) ? cout << "Prime" << endl :
+                      cout << "Not prime" << endl;
+        auto finish = high_resolution_clock::now();
+        auto d_ms = (duration<long, micro>) duration_cast<milliseconds>(finish - start);
+        cout << "n = " << n << " : " << d_ms.count() << "mcs" << endl;
     }
 
     return 0;
