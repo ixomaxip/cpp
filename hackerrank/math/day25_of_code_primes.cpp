758e64a3ac3525546e06a137b07f36096277a3db
Max <max.white.rabbit@gmail.com>
2020-04-26 23:03:36 +0500

    primes naive

---
+++ hackerrank/math/day25_of_code_primes.cpp
@@ -0,0 +1,60 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+// Sample Input
+//     3
+//     12
+//     5
+//     7
+
+// Sample Output
+//     Not prime
+//     Prime
+//     Prime
+
+vector<int> read_numbers() {
+    int N;
+    cin >> N;
+    vector<int> ns(N);
+    for (auto& n : ns) {
+        cin >> n;
+    }
+
+    return ns;
+}
+
+bool is_prime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            cout << i << endl;
+            return false;
+        }
+    }    
+    return true;
+
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // vector<int> ns = read_numbers();
+    vector<int> ns = {2038074743};
+
+    for (const auto& n : ns) {
+        auto start = steady_clock::now();
+        is_prime(n) ? cout <<"Prime" << endl :
+                      cout <<"Not prime" << endl;
+        auto finish = steady_clock::now();
+        cout << "n = " << n << " : " << duration_cast<milliseconds>(finish - start).count() << "ms" << endl;
+    }
+
+    return 0;
+}
