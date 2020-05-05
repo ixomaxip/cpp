33ba143e4296095fc7952048fa9e2f824c1e05af
Max <max.white.rabbit@gmail.com>
2020-05-05 23:30:41 +0500

    staircase

---
+++ hackerrank/algorithms/staircase.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the staircase function below.
+void staircase(int n) {
+    for (int i = 1; i <= n; i++) {
+        cout << string(n - i, ' ') << string(i, '#') << endl;
+    }
+
+
+}
+
+int main()
+{
+    int n;
+    // cin >> n;
+    n = 6;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    staircase(n);
+
+    return 0;
+}
