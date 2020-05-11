1c42ac8f02af70952542d2a14bd3257f3779f8e8
Max <max.white.rabbit@gmail.com>
2020-05-11 23:11:04 +0500

    kangaroo fix

--- hackerrank/algorithms/kangaroo.cpp
+++ hackerrank/algorithms/kangaroo.cpp
@@ -15,22 +15,26 @@ using namespace std;
 
 
 string kangaroo(int x1, int v1, int x2, int v2) {
-    if ((x1 - x2) * (v1 - v2) == 0) {
+    if (v1 > v2 && (x2 - x1) % (v1 - v2) == 0) {
         return "YES";
-    }
-    if ((x1 - x2) * (v1 - v2) > 0) {
+    } else {
         return "NO";
     }
-    return kangaroo(x1 + v1, v1, x2 + v2, v2);
+    // if ((x1 - x2) * (v1 - v2) > 0) {
+        // return "NO";
+    // }
+    // return kangaroo(x1 + v1, v1, x2 + v2, v2);
 }
 
 vector<vector<int>> tests() {
     vector<vector<int>> ret;
-    ret.push_back({2, 1, 1, 2});
-    ret.push_back({1, 1, 1, 1});
-    ret.push_back({0, 3, 4, 2});
-    ret.push_back({0, 2, 5, 3});
-    ret.push_back({0, 0, 5, 3});
+    // ret.push_back({2, 1, 1, 2});
+    // ret.push_back({1, 1, 2, 1});
+    // ret.push_back({0, 3, 4, 2});
+    // ret.push_back({0, 2, 5, 3});
+    // ret.push_back({0, 0, 5, 3});
+    ret.push_back({0, 2, 10000, 1});
+
 
     return ret;
 }
