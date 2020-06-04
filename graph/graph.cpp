69bcc700a9a8816a5009ea6b2f272d81ac861b84
Max <max.white.rabbit@gmail.com>
2020-06-04 23:36:53 +0500

    add nodes and edges

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -11,15 +11,51 @@ using namespace std;
 
 class Graph {
 public:
-    void add_node(const string& node_name){};
-    void add_edge(const string& from, const string& to) {};
-    vector<string> get_path(const string& from, const string& to ) {};
-    vector<vector<string>> get_cycles() const {};
+    void add_node(const string& node_name){
+        this->_adj[node_name] = {};
+    }
+    void add_edge(const string& from, const string& to) {
+        this->_adj[from].push_back(to);
+    }
+    vector<string> get_path(const string& from, const string& to ) {
+        vector<string> res;
+        return res;
+    }
+    vector<vector<string>> get_cycles() const {
+        vector<vector<string>> res;
+        return res;
+    }
+
+    void print() {
+        for (const auto& [key, value] : this->_adj) {
+            cout << key;
+            for (const auto& u : this->_adj[key]) {
+                cout << " -> " << u;
+            }
+            cout << endl;
+        }
+    };
+
+private:
+    map<string, vector<string>> _adj;
 };
 
 
 int main() {
-    cout << "start" << endl;
+
+    Graph g;
+    g.add_node("A");
+    g.add_node("B");
+    g.add_node("C");
+    g.add_node("D");
+
+    g.add_edge("A", "B");
+    g.add_edge("A", "C");
+    g.add_edge("D", "A");
+    g.add_edge("E", "C");
+
+
+    g.print();
 
     return 0;
 }
