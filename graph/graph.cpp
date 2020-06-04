e4d186c15a845ab6adc4be4d02624c3124dbf6e4
Max <max.white.rabbit@gmail.com>
2020-06-04 23:04:17 +0500

    prototype

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,10 +1,21 @@
 #include <bits/stdc++.h>
-#include <vector>
+#include <string>
+#include <map>
+#include <string>
+
 
 
 
 using namespace std;
 
+
+class Graph {
+public:
+    void add_node(const string& node_name){};
+    void add_edge(const string& from, const string& to) {};
+    vector<string> get_path(const string& from, const string& to ) {};
+    vector<vector<string>> get_cycles() const {};
+};
 
 
 int main() {
