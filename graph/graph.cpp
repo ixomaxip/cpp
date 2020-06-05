250602c108322bc98d92aacc2dea3d216d67ddb3
Max <max.white.rabbit@gmail.com>
2020-06-05 23:53:09 +0500

    get shortest path

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -2,11 +2,12 @@
 #include <string>
 #include <map>
 #include <string>
-
-
-
+#include <queue>
+#include <algorithm>
 
 using namespace std;
+
+
 
 
 class Graph {
@@ -14,15 +15,68 @@ public:
     void add_node(const string& node_name){
         this->_adj[node_name] = {};
     }
+
     void add_edge(const string& from, const string& to) {
         this->_adj[from].push_back(to);
         if (this->_adj.find(to) == this->_adj.end()) {
             this->add_node(to);
         }
     }
+    
     vector<string> get_path(const string& from, const string& to ) {
-        vector<string> res;
-        return res;
+        
+        queue<string> Q;
+        map<string, string> predecessor;
+        map<string, int> distance;
+        map<string, bool> visited;
+        // fill initials
+        for (const auto& [u, _] : this->_adj) {
+            predecessor[u] = "";
+            distance[u] = INT_MAX;
+            visited[u] = false;
+        }
+
+        visited[from] = true;
+        distance[from] = 0;
+        Q.push(from);
+        
+        //breadth-first search
+        bool found = false;
+        while (!Q.empty()) {
+            string u = Q.front();
+            Q.pop();
+            for (const auto& v : this->_adj[u]) {
+                if (visited[v] == false) {
+                    visited[v] = true;
+                    distance[v] = distance[u] + 1;
+                    predecessor[v] = u;
+                    Q.push(v);
+                }
+                if (v == to) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        //constructing path
+        vector<string> path;
+        if (!found) {
+            return path;
+        }
+        
+        string curr_node = to;
+        path.push_back(curr_node);
+        while (predecessor[curr_node] != "") {
+            path.push_back(predecessor[curr_node]);
+            curr_node = predecessor[curr_node];
+        }
+
+        reverse(path.begin(), path.end());
+
+        cout << "path: " << distance[to] << endl;
+
+        return path;
     }
     vector<vector<string>> get_cycles() const {
         vector<vector<string>> res;
